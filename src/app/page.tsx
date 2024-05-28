060677492d91766631da35794b69aed2733c4fae
José Ferrari <licoverpa321@gmail.com>
2024-05-27 23:09:23 -0300

    adding ednpoint response to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Teste from "./todos/page";
 
 export default function Home() {
   return (
     <main>
       <h1>Testing nextjs ;D</h1>
+      <Teste />
     </main>
   );
 }
--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 export default function Teste() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/todos")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+  console.log(data);
   return (
     <main>
       <h1>teste</h1>
