30ece1c506d4b9efe72f1d042bdd1ce60226f287
José Ferrari <licoverpa321@gmail.com>
2024-06-12 22:46:07 -0300

    adding user id group for todos

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,30 +1,50 @@
 "use client";
 
-import Sidebar from "@/components/Sidebar";
 import React, { useEffect, useState } from "react";
+import Sidebar from "@/components/Sidebar";
 
-interface Todos {
+interface Todo {
   id: number;
+  userId: number;
   title: string;
 }
 
+type GroupedTodos = Record<number, Todo[]>;
+
 export default function Todos() {
-  const [data, setData] = useState([]);
+  const [groupedTodos, setGroupedTodos] = useState<GroupedTodos>({});
 
   useEffect(() => {
     fetch("/api/todos")
       .then((response) => response.json())
-      .then((data) => setData(data));
+      .then((data) => {
+        const grouped = groupTodosByUserId(data);
+        setGroupedTodos(grouped);
+      });
   }, []);
-  console.log(data);
+
+  const groupTodosByUserId = (todos: Todo[]): GroupedTodos => {
+    return todos.reduce((acc, todo) => {
+      if (!acc[todo.userId]) {
+        acc[todo.userId] = [];
+      }
+      acc[todo.userId].push(todo);
+      return acc;
+    }, {} as GroupedTodos);
+  };
+
   return (
     <main>
       <Sidebar />
-      <h1>teste todos</h1>
-      {data?.map((e: Todos) => (
-        <div key={e.id} style={{ margin: 20 }}>
-          <p>{e.title}</p>
-        </div>
+      {Object.entries(groupedTodos).map(([userId, todos]) => (
+        <React.Fragment key={parseInt(userId)}>
+          <h2>User ID: {userId}</h2>
+          {todos.map((todo: Todo) => (
+            <div key={todo.id} style={{ margin: 20 }}>
+              <p>{todo.title}</p>
+            </div>
+          ))}
+        </React.Fragment>
       ))}
     </main>
   );
