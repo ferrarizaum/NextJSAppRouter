531f4a9c19a356fa625ae1f12ee1349b7bad177a
José Ferrari <licoverpa321@gmail.com>
2024-07-08 23:45:57 -0300

    Adding more tests

--- cypress/e2e/apiRequest.cy.ts
+++ cypress/e2e/apiRequest.cy.ts
@@ -5,4 +5,11 @@ describe("API Request Test", () => {
     cy.wait("@getTodos").its("response.statusCode").should("eq", 200);
   });
 });
+describe("API Request Test", () => {
+  it("successfully makes a request to the API", () => {
+    cy.intercept("GET", "/api/comments").as("getComments");
+    cy.visit("");
+    cy.wait("@getComments").its("response.statusCode").should("eq", 200);
+  });
+});
 //ADD MORE TESTS
